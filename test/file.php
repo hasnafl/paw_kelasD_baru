177520e684cd380805272db6676db8c375872ae1
hasnafl <33836437+hasnafl@users.noreply.github.com>
2018-01-09 02:31:51 +0700

    Update file.php

--- test/file.php
+++ test/file.php
@@ -10,10 +10,12 @@
 			$database = "klinik";
 			$connect = mysqli_connect($host,$user,$password,$database);
 			$login=mysqli_query($connect, "SELECT * FROM login where username = 'klinik1'");
-			if(mysqli_num_rows($login)==1){
+			if (!$check1_res) {
+    			printf("Error: %s\n", mysqli_error($con));
+    			exit();
+			}
 			$user = mysqli_fetch_array($login);
 			$test = $user['password'];
-			}
 			$content = $test;
             		$this->assertEquals('12345', $content);
 
