25bd9300f9a1821b14235b553fcd5aa635a9525a
hasnafl <33836437+hasnafl@users.noreply.github.com>
2018-01-08 23:52:40 +0700

    Update file.php

--- test/file.php
+++ test/file.php
@@ -1,25 +1,29 @@
 <?php
-	$host ="localhost";
-	$user = "root";
-	$password = "";
-	$database = "klinik";
-	$connect = mysqli_connect($host,$user,$password,$database);
 	
-
 	class file extends PHPUnit_Framework_TestCase
 	{
 		function test1()
     	{
-			$login=mysqli_query($connect,"SELECT * FROM login where username = 'klinik1'");
+    		$host ="localhost";
+			$user = "root";
+			$password = "";
+			$database = "klinik";
+			$connect = mysqli_connect($host,$user,$password,$database);
+			$login=mysqli_query($connect, "SELECT * FROM login where username = 'klinik1'");
 			$user = mysqli_fetch_array($login);
-			$test = $exe['password'];
+			$test = $user['password'];
 			$content = $test;
             $this->assertEquals('12345', $content);
 
 		}
 		function test2()
     	{
-			$login1=mysqli_query($connect,"SELECT * FROM login WHERE username = 'klinik1'");
+    		$host ="localhost";
+			$user = "root";
+			$password = "";
+			$database = "klinik";
+			$connect = mysqli_connect($host,$user,$password,$database);
+			$login1=mysqli_query($connect, "SELECT * FROM login WHERE username = 'klinik1'");
 			$user1 = mysqli_fetch_array($login1);
 			$hakakses = $user1['akses'];
 			$content = $hakakses;
