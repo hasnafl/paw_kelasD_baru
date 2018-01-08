4932343b7011cf527c0a761039050089b8025bd3
hasnafl <hasnalutfiyah@gmail.com>
2018-01-08 23:41:27 +0700

    xx

--- test/file.php
+++ test/file.php
@@ -4,10 +4,26 @@
 	$password = "";
 	$database = "klinik";
 	$connect = mysqli_connect($host,$user,$password,$database);
+	
 
-		$login=mysqli_query($connect,"SELECT * FROM login ");
-		$login1=mysqli_query($connect,"SELECT * FROM login WHERE username = 'klinik1'");
-		$user = mysqli_num_rows($login);
-		$user1 = mysqli_fetch_array($login1);
-		$hakakses = $user1['akses'];
+	class file extends PHPUnit_Framework_TestCase
+	{
+		function test1()
+    	{
+			$login=mysqli_query($connect,"SELECT * FROM login where username = 'klinik1'");
+			$user = mysqli_fetch_array($login);
+			$test = $exe['password'];
+			$content = $test;
+            $this->assertEquals('12345', $content);
+
+		}
+		function test2()
+    	{
+			$login1=mysqli_query($connect,"SELECT * FROM login WHERE username = 'klinik1'");
+			$user1 = mysqli_fetch_array($login1);
+			$hakakses = $user1['akses'];
+			$content = $hakakses;
+            $this->assertEquals('klinik1', $content);
+		}
+	}
 ?>
